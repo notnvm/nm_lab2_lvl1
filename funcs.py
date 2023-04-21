606cc7bbf8cd1791f0e85824f655e96dbef4f218
notnvm <auraz616@mail.ru>
2023-04-21 19:28:51 +0300

    fixed borders

--- funcs.py
+++ funcs.py
@@ -6,14 +6,14 @@ def u_test(x, y):
 def rsf_test(x, y):
     return (math.pi**2)*math.sin(math.pi*x*y)*(x**2+y**2)
 
-def mu1_test(a, y):
-    return u_test(a, y)
+def mu1_test(y):
+    return u_test(1, y)
 
-def mu2_test(b, y):
-    return u_test(b, y)
+def mu2_test(y):
+    return u_test(2, y)
 
-def mu3_test(x, c):
-    return u_test(x, c)
+def mu3_test(x):
+    return u_test(x, 2)
 
-def mu4_test(x, d):
-    return u_test(x, d)
+def mu4_test(x):
+    return u_test(x, 3)
